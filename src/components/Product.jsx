21a9f8c322887e333c33dc90bdd68b7c668419c1
Divi22005 <23r11a66b8@gcet.edu.in>
2025-06-03 09:48:02 +0530

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
+import { AppContext } from "../App";
+import axios from "axios";
 
-import React from 'react'
-import { useContext } from 'react'
-import { AppContext } from '../App'
 export default function Product() {
-  const {user} = useContext(AppContext)
+  const { user } = useContext(AppContext);
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const res = await axios.get("http://localhost:8080/products");
+      setProducts(res.data);
+    } catch (err) {
+      console.error("Error fetching products:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div>
-      <h3>Welcome {user.name}!</h3>
-      Product List
-      </div>
-  )
+    <div style={{ padding: "20px" }}>
+      <h3>Welcome {user.name || "Guest"}! </h3>
+      <h4>Product List:</h4>
+      <ul>
+        {products.map((item) => (
+          <li key={item.id}>
+            <strong>{item.name}</strong> - ${item.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
